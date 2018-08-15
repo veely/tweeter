d17306f7de3b0f2678774d46494ebfa7f01dcca2
Vincent Ly <veely1@gmail.com>
2018-08-15 22:19:49 +0000

    adds slide toggle

--- server/index.js
+++ server/index.js
@@ -29,10 +29,6 @@ const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 // Mount the tweets routes at the "/tweets" path prefix:
 app.use("/tweets", tweetsRoutes);
 
-app.post("/tweet", (req, res) => {
-
-});
-
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
