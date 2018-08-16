21aee5d04ddae7ccab509150416483f436619c02
Vincent Ly <veely1@gmail.com>
2018-08-16 17:52:43 +0000

    connects to mongoDB to fetch and save tweets

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -10,7 +10,7 @@ module.exports = function makeDataHelpers(db) {
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
       simulateDelay(() => {
-        db.tweets.push(newTweet);
+        db.collection("tweets").insertOne(newTweet);
         callback(null, true);
       });
     },
@@ -19,7 +19,20 @@ module.exports = function makeDataHelpers(db) {
     getTweets: function(callback) {
       simulateDelay(() => {
         const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
+
+        function fetchTweets(callback) {
+          db.collection("tweets").find().toArray((err, tweets) => {
+            if (err) {
+              return callback(err);
+            }
+            callback(null, tweets);
+          });
+        }
+
+        fetchTweets((err, tweets) => {
+          if (err) throw err;
+          callback(null, tweets.sort(sortNewestFirst));
+        });
       });
     }
 
